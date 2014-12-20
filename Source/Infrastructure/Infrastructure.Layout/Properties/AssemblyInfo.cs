499aa4dbde58f40cb5590d8b1c927ce72963cdfb
droid2015 <binhnt.ttdt@gmail.com>
2014-12-20 10:01:36 +0700

    khoi tao

---
+++ Source/Infrastructure/Infrastructure.Layout/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Infrastructure.Layout")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Infrastructure.Layout")]
+[assembly: AssemblyCopyright("Copyright © ")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("a934d0eb-9bdf-422c-96b3-5c30e973bfdb")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
